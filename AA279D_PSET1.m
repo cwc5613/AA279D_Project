e3fc656064837b0ca14f2a856db6919966245d00
Chris Covert <christopherwilliamcovert@gmail.com>
2018-04-25 14:40:52 -0700

    Add files via upload

--- AA279D_PSET1.m
+++ AA279D_PSET1.m
@@ -19,10 +19,10 @@ v0 = v_eci;
 r0 = r_eci;
 
 options = simset('SrcWorkspace','current');
-orbitCount = 1;
+orbitCount = 10;
 stopTime = orbitCount/revs_per_day*24*60*60;
 %stepSize = 24*60*60/revs_per_day/100000;
-stepSize = 5;
+stepSize = 1;
 timeSpan = [0:stepSize:stopTime]';
 
 sim('NUM_PROP',[],options);
@@ -67,15 +67,12 @@ for i = 1:size(err_r,2)
     [r_rtn2(:,i),v_rtn2(:,i)] = ECI2RTN(r_eci2(:,i), v_eci2(:,i));
     osc_h(:,i) = norm(cross(out_r_NUM_PROP(:,i),out_v_NUM_PROP(:,i)));
     osc_h_J2(:,i) = norm(cross(out_r_J2_Accel(:,i),out_v_J2_Accel(:,i)));
-    osc_hvec_J2(:,i) = cross(r_eci2(:,i),v_eci2(:,i));
-    osc_evec_J2(:,i) = ((norm(v_eci2(:,i))^2/mu)-(1/norm(r_eci2(:,i))))*...
-        r_eci2(:,i)-(dot(r_eci2(:,i),v_eci2(:,i)))*v_eci2(:,i)/mu;
 end
 
 % err_r_rtn = [r_rtn1(1,:)-r_rtn2(1,:);...
 %     r_rtn1(2,:)-r_rtn2(2,:);...
 %     r_rtn1(3,:)-r_rtn2(3,:)];
-% 
+%
 % err_v_rtn = [v_rtn1(1,:)-v_rtn2(1,:);...
 %     v_rtn1(2,:)-v_rtn2(2,:);...
 %     v_rtn1(3,:)-v_rtn2(3,:)];
@@ -107,7 +104,7 @@ end
 % subplot(3,1,1); plot(err_r_rtn(1,:)); title(''); ylabel('Radial Position Error (km)');
 % subplot(3,1,2); plot(err_r_rtn(2,:)); title(''); ylabel('Tangential Position Error (km)');
 % subplot(3,1,3); plot(err_r_rtn(3,:)); title(''); ylabel('Normal Position Error (km)');
-% 
+%
 % figure('color',[1 1 1]);
 % subplot(3,1,1); plot(err_v_rtn(1,:)); title(''); ylabel('Radial Velocity Error (km/s)');
 % subplot(3,1,2); plot(err_v_rtn(2,:)); title(''); ylabel('Tangential Velocity Error (km/s)');
@@ -122,7 +119,7 @@ RAAN_bar = mean(osc_RAAN_J2);
 w_bar = mean(osc_w_J2);
 anom_bar = mean(osc_anom_J2);
 
-sim('MeanOE',[],options);
+sim('MeanOE17',[],options);
 
 R2D = 180/pi;
 
@@ -133,47 +130,86 @@ mean_RAAN = RAAN + R2D*mean_dRAAN.*timeSpan;
 mean_w = w + R2D*mean_dw.*timeSpan;
 mean_anom = anom + R2D*mean_danom.*timeSpan;
 
+for i =1:size(err_r,2)
+    osc_evec(:,i) = ((norm(out_v_NUM_PROP(:,i))^2/mu)-(1/norm(out_r_NUM_PROP(:,i))))*...
+        out_r_NUM_PROP(:,i)-(dot(out_r_NUM_PROP(:,i),out_v_NUM_PROP(:,i)))*out_v_NUM_PROP(:,i)/mu;
+    osc_evec_J2(:,i) = ((norm(out_v_J2_Accel(:,i))^2/mu)-(1/norm(out_r_J2_Accel(:,i))))*...
+        out_r_J2_Accel(:,i)-(dot(out_r_J2_Accel(:,i),out_v_J2_Accel(:,i)))*out_v_J2_Accel(:,i)/mu;
+    osc_hvec(:,i) = cross(out_r_NUM_PROP(:,i),out_v_NUM_PROP(:,i));
+    osc_hvec_J2(:,i) = cross(out_r_J2_Accel(:,i),out_v_J2_Accel(:,i));
+    
+end
+
 %%
 
 figure
-title('Unperturbed Osculating Orbital Elements')
-subplot(4,2,1);plot(osc_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
-subplot(4,2,2);plot(osc_e);xlabel('Time (sec)');ylabel('Eccentricity');
-subplot(4,2,3);plot(osc_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');
-subplot(4,2,4);plot(osc_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');
-subplot(4,2,5);plot(osc_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');
-subplot(4,2,6);plot(osc_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');
-subplot(4,2,7);plot(osc_h);xlabel('Time (sec)');ylabel('Angular Momentum (km^2/s)');
-subplot(4,2,8);plot(osc_ME);xlabel('Time (sec)');ylabel('Specific Mech. Energy (km^2/s^2)');
+subplot(6,1,1);hold on; plot(osc_a_J2);plot(osc_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');hold off;
+subplot(6,1,2);hold on; plot(osc_e_J2);plot(osc_e);xlabel('Time (sec)');ylabel('Eccentricity');hold off;
+subplot(6,1,3);hold on; plot(osc_inc_J2);plot(osc_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');hold off;
+subplot(6,1,4);hold on; plot(osc_RAAN_J2);plot(osc_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');hold off;
+subplot(6,1,5);hold on; plot(osc_w_J2);plot(osc_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');hold off;
+subplot(6,1,6);hold on; plot(osc_anom_J2);plot(osc_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');hold off;
 
 figure
 title('Osculating Orbital Elements with J2 Effects')
-subplot(3,1,1);plot(osc_evec_J2(1,:));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
-subplot(3,1,2);plot(osc_evec_J2(2,:));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
-subplot(3,1,3);plot(osc_evec_J2(3,:));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+subplot(3,1,1);hold on; plot(osc_evec(1,:));plot(osc_evec_J2(1,:));xlabel('Time (sec)');ylabel('e_x');hold off;
+subplot(3,1,2);hold on; plot(osc_evec(2,:));plot(osc_evec_J2(2,:));xlabel('Time (sec)');ylabel('e_y');hold off;
+subplot(3,1,3);hold on; plot(osc_evec(3,:));plot(osc_evec_J2(3,:));xlabel('Time (sec)');ylabel('e_z');hold off;
 
 figure
 title('Osculating Orbital Elements with J2 Effects')
-subplot(3,1,1);plot(osc_hvec_J2(1,2:end));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
-subplot(3,1,2);plot(osc_hvec_J2(2,2:end));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
-subplot(3,1,3);plot(osc_hvec_J2(3,2:end));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+subplot(4,1,1);hold on; plot(osc_hvec(1,:));plot(osc_hvec_J2(1,:));xlabel('Time (sec)');ylabel('h_x (km^2/s)');hold off;
+subplot(4,1,2);hold on; plot(osc_hvec(2,:));plot(osc_hvec_J2(2,:));xlabel('Time (sec)');ylabel('h_y (km^2/s)');hold off;
+subplot(4,1,3);hold on; plot(osc_hvec(3,:));plot(osc_hvec_J2(3,:));xlabel('Time (sec)');ylabel('h_z (km^2/s)');hold off;
+subplot(4,1,4);hold on; plot(osc_ME);plot(osc_ME_J2);xlabel('Time (sec)');ylabel('\epsilon (km^2/s^2)');hold off;
 
 figure
-title('Unperturbed Osculating Orbital Elements')
-subplot(4,2,1);plot(osc_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
-subplot(4,2,2);plot(osc_e);xlabel('Time (sec)');ylabel('Eccentricity');
-subplot(4,2,3);plot(osc_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');
-subplot(4,2,4);plot(osc_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');
-subplot(4,2,5);plot(osc_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');
-subplot(4,2,6);plot(osc_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');
-subplot(4,2,7);plot(osc_h);xlabel('Time (sec)');ylabel('Angular Momentum (km^2/s)');
-subplot(4,2,8);plot(osc_ME);xlabel('Time (sec)');ylabel('Specific Mech. Energy (km^2/s^2)');
+subplot(6,1,1);hold on; plot(osc_a_J2);plot(mean_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');hold off;
+subplot(6,1,2);hold on; plot(osc_e_J2);plot(mean_e);xlabel('Time (sec)');ylabel('Eccentricity');hold off;
+subplot(6,1,3);hold on; plot(osc_inc_J2);plot(mean_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');hold off;
+subplot(6,1,4);hold on; plot(osc_RAAN_J2);plot(mean_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');hold off;
+subplot(6,1,5);hold on; plot(osc_w_J2);plot(mean_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');hold off;
+subplot(6,1,6);hold on; plot(osc_anom_J2);plot(mean_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');hold off;
 
-figure
-subplot(4,2,1);hold on; plot(osc_a_J2);plot(mean_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');hold off;
-subplot(4,2,2);hold on; plot(osc_e_J2);plot(mean_e);xlabel('Time (sec)');ylabel('Eccentricity');hold off;
-subplot(4,2,3);hold on; plot(osc_inc_J2);plot(mean_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');hold off;
-subplot(4,2,4);hold on; plot(osc_RAAN_J2);plot(mean_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');hold off;
-subplot(4,2,5);hold on; plot(osc_w_J2);plot(mean_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');hold off;
-subplot(4,2,6);hold on; plot(osc_anom_J2);plot(mean_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');hold off;
-
+% figure
+% title('Unperturbed Osculating Orbital Elements')
+% subplot(4,2,1);plot(osc_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+% subplot(4,2,2);plot(osc_e);xlabel('Time (sec)');ylabel('Eccentricity');
+% subplot(4,2,3);plot(osc_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');
+% subplot(4,2,4);plot(osc_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');
+% subplot(4,2,5);plot(osc_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');
+% subplot(4,2,6);plot(osc_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');
+% subplot(4,2,7);plot(osc_h);xlabel('Time (sec)');ylabel('Angular Momentum (km^2/s)');
+% subplot(4,2,8);plot(osc_ME);xlabel('Time (sec)');ylabel('Specific Mech. Energy (km^2/s^2)');
+%
+% figure
+% title('Osculating Orbital Elements with J2 Effects')
+% subplot(3,1,1);plot(osc_evec_J2(1,:));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+% subplot(3,1,2);plot(osc_evec_J2(2,:));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+% subplot(3,1,3);plot(osc_evec_J2(3,:));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+%
+% figure
+% title('Osculating Orbital Elements with J2 Effects')
+% subplot(3,1,1);plot(osc_hvec_J2(1,2:end));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+% subplot(3,1,2);plot(osc_hvec_J2(2,2:end));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+% subplot(3,1,3);plot(osc_hvec_J2(3,2:end));xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+%
+% figure
+% title('Unperturbed Osculating Orbital Elements')
+% subplot(4,2,1);plot(osc_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');
+% subplot(4,2,2);plot(osc_e);xlabel('Time (sec)');ylabel('Eccentricity');
+% subplot(4,2,3);plot(osc_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');
+% subplot(4,2,4);plot(osc_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');
+% subplot(4,2,5);plot(osc_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');
+% subplot(4,2,6);plot(osc_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');
+% subplot(4,2,7);plot(osc_h);xlabel('Time (sec)');ylabel('Angular Momentum (km^2/s)');
+% subplot(4,2,8);plot(osc_ME);xlabel('Time (sec)');ylabel('Specific Mech. Energy (km^2/s^2)');
+%
+% figure
+% subplot(4,2,1);hold on; plot(osc_a_J2);plot(mean_a);xlabel('Time (sec)');ylabel('Semimajor Axis (km)');hold off;
+% subplot(4,2,2);hold on; plot(osc_e_J2);plot(mean_e);xlabel('Time (sec)');ylabel('Eccentricity');hold off;
+% subplot(4,2,3);hold on; plot(osc_inc_J2);plot(mean_inc);xlabel('Time (sec)');ylabel('Inclination (\circ)');hold off;
+% subplot(4,2,4);hold on; plot(osc_RAAN_J2);plot(mean_RAAN);xlabel('Time (sec)');ylabel('RAAN (\circ)');hold off;
+% subplot(4,2,5);hold on; plot(osc_w_J2);plot(mean_w);xlabel('Time (sec)');ylabel('Arg. of Periapsis (\circ)');hold off;
+% subplot(4,2,6);hold on; plot(osc_anom_J2);plot(mean_anom);xlabel('Time (sec)');ylabel('True Anomaly (\circ)');hold off;
+%
--- AA279D_PSET2_a_change.m
+++ AA279D_PSET2_a_change.m
@@ -14,7 +14,7 @@ tol = 10^-5;
 D2R = pi/180;
 R2D = 1/D2R;
 
-delta_a = 4;
+delta_a = 2;
 
 a2 = a+delta_a;
 e2 = e;
@@ -47,11 +47,11 @@ rel_pos0 = [rho_RTN_init;r0;th0];
 rel_vel0 = [rhod_RTN_init;rd0;thd0];
 
 options = simset('SrcWorkspace','current');
-orbitCount = 4;
+orbitCount = 10;
 stopTime = orbitCount/revs_per_day*24*60*60;
-stepSize = 1;
+stepSize = 5;
 impulseTime = 0;
-deltaV_theta = 0;
+dV = [0;0;0];
 sim('relative_orbit_prop',[],options);
 
 rel_pos_RTN = squeeze(rel_pos_RTN);
@@ -75,38 +75,83 @@ dep_r_ECI = reshape(NUM_r,[size(NUM_r,1),size(NUM_r,3)]);
 dep_v_ECI = reshape(NUM_v,[size(NUM_v,1),size(NUM_v,3)]);
 
 for i = 1:size(ch_r_ECI,2)
-%     ch_r = ch_r_ECI(:,i)/norm(ch_r_ECI(:,i));
-%     ch_h = cross(ch_r_ECI(:,i),ch_v_ECI(:,i))/...
-%         norm(cross(ch_r_ECI(:,i),ch_v_ECI(:,i)));
+    %     ch_r = ch_r_ECI(:,i)/norm(ch_r_ECI(:,i));
+    %     ch_h = cross(ch_r_ECI(:,i),ch_v_ECI(:,i))/...
+    %         norm(cross(ch_r_ECI(:,i),ch_v_ECI(:,i)));
     ch_T_ECI2RTN = ECI2RTN_rel(ch_r_ECI(:,i),ch_v_ECI(:,i));
     ch_r_rtn(:,i) = ch_T_ECI2RTN*(dep_r_ECI(:,i)-ch_r_ECI(:,i));
     ch_v_rtn(:,i) = ch_T_ECI2RTN*(dep_v_ECI(:,i)-ch_v_ECI(:,i))-...
         cross([0;0;norm(cross(ch_r_ECI(:,i),ch_v_ECI(:,i)))/...
         (norm(ch_r_ECI(:,i))^2)], ch_r_rtn(:,i));
-    apoapsis_finder(:,i) = norm(dep_r_ECI(:,i));
+    min_finder(:,i) = norm(rel_pos_RTN(:,i));
 end
 
 figure
-hold on
-plot3(ch_r_rtn(1,:),ch_r_rtn(2,:),ch_r_rtn(3,:),'r--','LineWidth',3)
-plot3(rel_pos_RTN(1,:),rel_pos_RTN(2,:),rel_pos_RTN(3,:),'LineWidth',1.5)
-hold off
+title('Osculating Orbital Elements with J2 Effects')
+subplot(3,1,1);hold on;plot(rel_pos_RTN(1,:),'LineWidth',1.5);plot(ch_r_rtn(1,:),'r--','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta r_R (km)');
+subplot(3,1,2);hold on;plot(rel_pos_RTN(2,:),'LineWidth',1.5);plot(ch_r_rtn(2,:),'r--','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta r_T (km)');
+subplot(3,1,3);hold on;plot(rel_pos_RTN(3,:),'LineWidth',1.5);plot(ch_r_rtn(3,:),'r--','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta r_N (km)');
 
-dep_h = norm(cross(r2_ECI_init,v2_ECI_init));
-dep_r = norm(r2_ECI_init);
-dep_p = a2*(1-e2^2);
+figure
+title('Osculating Orbital Elements with J2 Effects')
+subplot(3,1,1);hold on;plot(rel_vel_RTN(1,:),'LineWidth',1.5);plot(ch_v_rtn(1,:),'r--','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta v_R (km/s)');
+subplot(3,1,2);hold on;plot(rel_vel_RTN(2,:),'LineWidth',1.5);plot(ch_v_rtn(2,:),'r--','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta v_T (km/s)');
+subplot(3,1,3);hold on;plot(rel_vel_RTN(3,:),'LineWidth',1.5);plot(ch_v_rtn(3,:),'r--','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta v_N (km/s)');
 
-deltaV_theta = (delta_a*dep_r*dep_h)/(2*a2^2*dep_p)
-[apoapsis,impulseTime] = min(apoapsis_finder)
+% DELTA_V -------------------------------------------------
+
+tout = 1:stepSize:stopTime;
+
+x = rel_pos_RTN(1,:); dx = rel_vel_RTN(1,:);
+y = rel_pos_RTN(2,:); dy = rel_vel_RTN(2,:);
+z = rel_pos_RTN(3,:); dz = rel_vel_RTN(3,:);
+r = rel_pos_RTN(4,:); dr = rel_vel_RTN(4,:);
+th = rel_pos_RTN(5,:); dth = rel_vel_RTN(5,:);
+
+Tc = 1/revs_per_day*24*60*60;
+T1 = find(tout>0.99*Tc,1);
+
+for t = 1:T1
+    Vxm = dx(t)-dth(t)*y(t)+dr(t);
+    Vym = dy(t)+dth(t)*(x(t)+r(t));
+    Vzm = dz(t);
+    r1 = sqrt((r(t)+x(t))^2+y(t)^2+z(t)^2);
+    v1 = sqrt(Vxm^2+Vym^2+Vzm^2);
+    term = -1+1/v1*sqrt(mu*(2*a-r1)/(a*r1));
+    DVx = Vxm*term;
+    DVy = Vym*term;
+    DVz = Vzm*term;
+    DVtot(t) = sqrt(DVx^2+DVy^2+DVz^2);
+end
+
+impulseTime = tout(DVtot == min(DVtot));
+loc = find(DVtot == min(DVtot));
+
+Vxm = dx(loc)-dth(loc)*y(loc)+dr(loc);
+Vym = dy(loc)+dth(loc)*(x(loc)+r(loc));
+Vzm = dz(loc);
+r1 = sqrt((r(loc)+x(loc))^2+y(loc)^2+z(loc)^2);
+v1 = sqrt(Vxm^2+Vym^2+Vzm^2);
+term = -1+1/v1*sqrt(mu*(2*a-r1)/(a*r1));
+DVx = Vxm*term;
+DVy = Vym*term;
+DVz = Vzm*term;
+
+dV = [DVx;DVy;DVz]/stepSize
 
 sim('relative_orbit_prop',[],options);
 
 rel_pos_RTN = squeeze(rel_pos_RTN);
 rel_vel_RTN = squeeze(rel_vel_RTN);
 
-ENERGY1 = norm(v_ECI_init)^2/2-mu/norm(r_ECI_init);
-ENERGY2 = norm(rel_vel_RTN(1:3,end))^2/2-...
-    mu/ norm(rel_pos_RTN(1:3,end));
+figure
+plot(tout(1:T1)/Tc,DVtot)
+
+figure
+title('Osculating Orbital Elements with J2 Effects')
+subplot(3,1,1);hold on;plot(tout,rel_pos_RTN(1,:),'LineWidth',1.5);plot(tout(loc),rel_pos_RTN(1,loc),'ro','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta r_R (km)');
+subplot(3,1,2);hold on;plot(tout,rel_pos_RTN(2,:),'LineWidth',1.5);plot(tout(loc),rel_pos_RTN(2,loc),'ro','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta r_T (km)');
+subplot(3,1,3);hold on;plot(tout,rel_pos_RTN(3,:),'LineWidth',1.5);plot(tout(loc),rel_pos_RTN(3,loc),'ro','LineWidth',3);hold off;xlabel('Time (sec)');ylabel('\delta r_N (km)');
 
 figure
 plot3(rel_pos_RTN(1,:),rel_pos_RTN(2,:),rel_pos_RTN(3,:),'LineWidth',2)
